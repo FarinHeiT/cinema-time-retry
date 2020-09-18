1b6fb05c87ae46b1a759b610af1a485242cbc860
ilya83065 <ilyathomas8306@icloud.com>
2020-09-18 16:19:13 -0400

    password start

--- cinema_time_retry_in/app/routes.py
+++ cinema_time_retry_in/app/routes.py
@@ -1,8 +1,9 @@
 import json
 
-from flask import Flask, render_template, Blueprint, url_for
+from flask import Flask, render_template, Blueprint, url_for,redirect
 
 from cinema_time_retry_in import socketio, redis_db
+from . import forms
 
 from .helper_functions import generate_room_name
 
@@ -18,7 +19,18 @@ def index():
 def room(room_name):
     link = json.loads(redis_db.get(room_name))['playlist'][0]
     print(link)
-    return render_template('room.html', video_link=link, source='youtube')
+    password = json.loads(redis_db.get(room_name)["password"])
+    return render_template('room.html', video_link=link, source='youtube', password=password)
+
+@general.route("/password")
+def password_in(data):
+    form = forms.password_form()
+    if form.validate_on_submit():
+        if form.password.data == data['password']:
+            socketio.emit("create_room")
+        else:
+            return redirect(url_for('password_in', data=data))
+    return render_template("password.html", form=form)
 
 
 @socketio.on('connection')
@@ -26,14 +38,23 @@ def connection(json):
     print('New user connected!' + str(json))
 
 
+@socketio.on("t_password")
+def type_in_password(data):
+    return redirect(url_for('general.password_in', data=data))
+
+
+
 @socketio.on('create_room')
 def create_room(data):
     room_name = generate_room_name()
 
     room = {
-        'playlist': [data['videoLink']]
+        'playlist': [data['videoLink']],
+        'password': [data['password']]
     }
 
     redis_db.set(room_name, json.dumps(room))
 
     socketio.emit('redirect', {'url': url_for('general.room', room_name=room_name)})
+
+
--- cinema_time_retry_in/static/js/main.js
+++ cinema_time_retry_in/static/js/main.js
@@ -12,9 +12,10 @@ const createRoom = document.querySelector('button[name="createRoom"]');
 
 createRoom.addEventListener('click', () => {
     let videoLink = document.querySelector('#videoLink').value;
+    let password = document.querySelector('#password').value;
 
     socket.emit('create_room', {
-        videoLink
+        videoLink, password
     });
 });
 
