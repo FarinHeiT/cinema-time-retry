1be659c1a575bb9e629bc40f5d32e8a1ef621648
ilya83065 <ilyathomas8306@icloud.com>
2020-09-19 10:39:36 -0400

    joinroom + password

--- cinema_time_retry_in/app/routes.py
+++ cinema_time_retry_in/app/routes.py
@@ -1,9 +1,11 @@
 import json
 
-from flask import Flask, render_template, Blueprint, url_for,redirect
+from flask import Flask, render_template, Blueprint, url_for,redirect, session, request
 
 from cinema_time_retry_in import socketio, redis_db
 from . import forms
+
+from flask_socketio import join_room
 
 from .helper_functions import generate_room_name
 
@@ -12,22 +14,29 @@ general = Blueprint('general', __name__)
 
 @general.route('/')
 def index():
-    return render_template("Main.html")
+    form = forms.name_form()
+    if form.validate_on_submit():
+        session['username'] = form.name.data
+    return render_template("Main.html", form=form)
 
 
 @general.route('/room/<room_name>')
-def room(room_name):
-    link = json.loads(redis_db.get(room_name))['playlist'][0]
-    print(link)
-    password = json.loads(redis_db.get(room_name)["password"])
-    return render_template('room.html', video_link=link, source='youtube', password=password)
+def room(room_name, data):
+    if request.sid in json.loads(redis_db.get(room_name))['users']:
+        join_room(room_name)
+        link = json.loads(redis_db.get(room_name))['playlist'][0]
+        print(link)
+        password = json.loads(redis_db.get(room_name))['password']
+        return render_template('room.html', video_link=link, source='youtube', password=password)
+    else:
+        socketio.emit('redirect', {'url': url_for('general.password', data=data)})
 
 @general.route("/password")
 def password_in(data):
     form = forms.password_form()
     if form.validate_on_submit():
         if form.password.data == data['password']:
-            socketio.emit("create_room")
+            socketio.emit("")
         else:
             return redirect(url_for('password_in', data=data))
     return render_template("password.html", form=form)
@@ -38,9 +47,9 @@ def connection(json):
     print('New user connected!' + str(json))
 
 
-@socketio.on("t_password")
-def type_in_password(data):
-    return redirect(url_for('general.password_in', data=data))
+#@socketio.on("t_password")
+#def type_in_password(data):
+#    socketio.emit('redirect', {'url': url_for('general.password', data=data)})
 
 
 
@@ -50,11 +59,11 @@ def create_room(data):
 
     room = {
         'playlist': [data['videoLink']],
-        'password': [data['password']]
+        'password': data['password'],
+        'users' : []
     }
 
     redis_db.set(room_name, json.dumps(room))
-
-    socketio.emit('redirect', {'url': url_for('general.room', room_name=room_name)})
+    socketio.emit('redirect', {'url': url_for('general.room', room_name=room_name, data=data)})
 
 
