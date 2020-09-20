b4b2fa1ae6c8561d26de659a61b71c24b8a450c1
FarinHeiT <yariksuharik85@gmail.com>
2020-09-20 18:10:39 +0300

    Socket.io logger only Errors display mode

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import logging
+
+
 class Config:
     SECRET_KEY = "TOPSECRETKEY!"
     WTF_CSRF_SECRET_KEY = 'TOPSECRETKEY!'
+    logging.getLogger('socketio').setLevel(logging.ERROR)
+    logging.getLogger('engineio').setLevel(logging.ERROR)
