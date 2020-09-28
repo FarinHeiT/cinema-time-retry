f3b5536d2f1b13abe662e80cac2b7ca4e7b566ca
ilya83065 <ilyathomas8306@icloud.com>
2020-09-28 10:51:17 -0400

    all works

--- cinema_time_retry_in/__init__.py
+++ cinema_time_retry_in/__init__.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO
 import config
 import redis
 
-socketio = SocketIO(ping_interval=1, ping_timeout=2)
+socketio = SocketIO()
 redis_db = redis.Redis(host='localhost', port=6379, db=0)
 
 
