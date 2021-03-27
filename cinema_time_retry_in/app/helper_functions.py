662a3315c581e9edab39802f1cfd55ffbaf99e80
farinheit <yariksuharik85@gmail.com>
2021-03-27 16:59:52 +0200

    halfmoon connection, base template

--- cinema_time_retry_in/app/helper_functions.py
+++ cinema_time_retry_in/app/helper_functions.py
@@ -6,26 +6,25 @@ from bs4 import BeautifulSoup as BS
 
 
 def generate_room_name():
-	words = ("Haphazard", "Hodgepodge", "Hogwash", "Hoodwink", "Hubbub", "Itty-Bitty"
-		, "Pompous", "Rambunctious", "Ramshackle", "Shenanigans", "Shrubs", "Skedaddle"
-		, "Squabble", "Squeegee", "Squelch", "Wishy-Washy", "Shrubbery", "Taradiddle"
-		, "Snickersnee", "Widdershins", "Collywobbles", "Gubbins", "Bumfuzzle", "Cattywampus"
-		, "Hatskers")
+    words = ("Haphazard", "Hodgepodge", "Hogwash", "Hoodwink", "Hubbub", "Itty-Bitty"
+             , "Pompous", "Rambunctious", "Ramshackle", "Shenanigans", "Shrubs", "Skedaddle"
+             , "Squabble", "Squeegee", "Squelch", "Wishy-Washy", "Shrubbery", "Taradiddle"
+             , "Snickersnee", "Widdershins", "Collywobbles", "Gubbins", "Bumfuzzle", "Cattywampus"
+             , "Hatskers")
 
-	first = random.choice(words)
-	seconond = random.choice(words)
-	word = first + seconond + str(random.randint(0, 9999))
+    first = random.choice(words)
+    seconond = random.choice(words)
+    word = first + seconond + str(random.randint(0, 9999))
 
-	return word
+    return word
 
 
 # DELETES EVERYTING FROM REDIS!!!
 def clear_redis_db():
+    redis_db = redis.Redis(host='localhost', port=6379, db=0)
 
-	redis_db = redis.Redis(host='localhost', port=6379, db=0)
-
-	for i in redis_db.scan_iter():
-		redis_db.delete(i)
+    for i in redis_db.scan_iter():
+        redis_db.delete(i)
 
 
 def get_youtube_title(url):
