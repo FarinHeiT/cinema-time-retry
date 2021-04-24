50a7f0cb12af50953a9785d2f368e08342248a6c
ilya83065 <ilyathomas8306@icloud.com>
2021-04-24 09:49:22 -0400

    create room pop-up

--- cinema_time_retry_in/static/js/main.js
+++ cinema_time_retry_in/static/js/main.js
@@ -19,3 +19,40 @@ createRoom.addEventListener('click', () => {
 socket.on('redirect', function (data) {
     window.location = data.url;
 });
+
+// Modal window
+window.onload = function () {
+    const openRoomSettings = document.querySelector(".pure-button")
+    const closeRoomSettings = document.querySelector(".modal-close")
+    const overlay = document.querySelector('#modal-overlay')
+    const checkbox = document.querySelector('#private-room-checkbox')
+    const password = document.querySelector('#invisible')
+
+    openRoomSettings.onclick = function () {
+        const modal = document.querySelector('#room-settings')
+        modal.classList.add('active')
+        overlay.classList.add('active')
+
+
+    }
+
+    closeRoomSettings.onclick = function () {
+        const modal = document.querySelector('#room-settings')
+        modal.classList.remove('active')
+        overlay.classList.remove('active')
+    }
+
+    overlay.onclick = function () {
+        const modal = document.querySelector('#room-settings')
+        modal.classList.remove('active')
+        overlay.classList.remove('active')
+    }
+
+    checkbox.onclick = function () {
+        if (checkbox.checked == true) {
+            password.classList.add('active')
+        } else {
+            password.classList.remove('active')
+        }
+    }
+}
