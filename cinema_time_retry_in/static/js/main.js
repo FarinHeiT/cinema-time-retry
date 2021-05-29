9f51937f97b812031aa0ac14d1ad24785a3ac2d4
ilya2 <ilya228888u@gmail.com>
2021-05-29 10:14:22 -0500

    undone data tables code

--- cinema_time_retry_in/static/js/main.js
+++ cinema_time_retry_in/static/js/main.js
@@ -15,3 +15,20 @@ createRoom.addEventListener('submit', (e) => {
 socket.on('error', (data)=>{
     createToast(data.title, data.content, "alert-danger")
 })
+
+setInterval(function () {
+    socket.emit("get_rooms_data")
+}, 10000)
+
+socket.on("new_room_data", (data)=>{
+    console.log(data)
+    data = JSON.parse(data)
+    $('#example').DataTable( {
+        bSort: false,
+        data: data,
+        columns: [
+            { title: "Name" },
+            { title: "Link" }
+        ]
+    } );
+})
